924e851b8cb11beeae3219db556907224c0ba5eb
Harold Mesa <harold.mesa@icloud.com>
2022-08-09 08:36:46 -0600

    WIP: Adding different mathematical operations and alert messages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-// business logic
+////// business logic
 function add(number1, number2) {
     return number1 + number2;
   }
-  
+
+// user interface logic 
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+    
+window.alert("The addition of your numbers equals " + add(number1, number2));
+
+////// business logic
+function subtract(number3, number4) {
+    return number3 - number4;
+  }
+
   // user interface logic 
-  const number1 = prompt("Enter a number:");
-  const number2 = prompt("Enter another number:");
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+    
+window.alert("The subtraction of your numbers equals " + subtract(number3, number4));
+
+////// business logic
+
+/* function multiply(number1, number2) {
+    return number1 * number2;
+  }
+
+function divide(number1, number2) {
+    return number1 / number2;
+  }
   
-  window.alert(add(number1, number2));
+
+
+  window.alert(subtract(number2, number3));
+  window.alert(multiply(number1, number2));
+  window.alert(divide(number1, number2)); */
